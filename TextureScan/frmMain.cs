beaf50dd89ebb6524d4acbe61c8625291eb92c1c
Niston Cloud <niston.cloud@outlook.com>
2023-05-24 20:35:59 +0200

    Sort/remove usings

--- TextureScan/frmMain.cs
+++ TextureScan/frmMain.cs
@@ -1,17 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
-using System.Drawing;
 using System.Globalization;
 using System.IO;
-using System.Linq;
-using System.Net;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace TextureScan

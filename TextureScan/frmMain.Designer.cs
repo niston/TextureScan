043129c4ffec62909c057fc84fe7f45df5c172a6
Niston Cloud <niston.cloud@outlook.com>
2023-05-24 20:28:51 +0200

    Add project files.

---
+++ TextureScan/frmMain.Designer.cs
@@ -0,0 +1,210 @@
+﻿
+namespace TextureScan
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tslStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtScanPath = new System.Windows.Forms.TextBox();
+            this.lsvResults = new System.Windows.Forms.ListView();
+            this.colFilePath = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colDimensions = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colMipmapCount = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colIsCubemap = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tslStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 396);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(952, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tslStatus
+            // 
+            this.tslStatus.Name = "tslStatus";
+            this.tslStatus.Size = new System.Drawing.Size(937, 17);
+            this.tslStatus.Spring = true;
+            this.tslStatus.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.lsvResults);
+            this.splitContainer1.Size = new System.Drawing.Size(952, 396);
+            this.splitContainer1.SplitterDistance = 51;
+            this.splitContainer1.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnGo);
+            this.groupBox1.Controls.Add(this.btnBrowse);
+            this.groupBox1.Controls.Add(this.txtScanPath);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(952, 51);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Scan Folder";
+            // 
+            // btnGo
+            // 
+            this.btnGo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGo.Location = new System.Drawing.Point(893, 20);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(46, 20);
+            this.btnGo.TabIndex = 2;
+            this.btnGo.Text = "GO";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowse.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowse.Location = new System.Drawing.Point(859, 20);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(28, 20);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtScanPath
+            // 
+            this.txtScanPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtScanPath.Location = new System.Drawing.Point(13, 20);
+            this.txtScanPath.Name = "txtScanPath";
+            this.txtScanPath.Size = new System.Drawing.Size(840, 20);
+            this.txtScanPath.TabIndex = 0;
+            // 
+            // lsvResults
+            // 
+            this.lsvResults.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colFilePath,
+            this.colDimensions,
+            this.colMipmapCount,
+            this.colIsCubemap});
+            this.lsvResults.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lsvResults.FullRowSelect = true;
+            this.lsvResults.HideSelection = false;
+            this.lsvResults.Location = new System.Drawing.Point(0, 0);
+            this.lsvResults.Name = "lsvResults";
+            this.lsvResults.Size = new System.Drawing.Size(952, 341);
+            this.lsvResults.TabIndex = 0;
+            this.lsvResults.UseCompatibleStateImageBehavior = false;
+            this.lsvResults.View = System.Windows.Forms.View.Details;
+            this.lsvResults.DoubleClick += new System.EventHandler(this.lsvResults_Click);
+            // 
+            // colFilePath
+            // 
+            this.colFilePath.Text = "Texture Path";
+            this.colFilePath.Width = 700;
+            // 
+            // colDimensions
+            // 
+            this.colDimensions.Text = "Dimensions";
+            this.colDimensions.Width = 100;
+            // 
+            // colMipmapCount
+            // 
+            this.colMipmapCount.Text = "MipMaps";
+            // 
+            // colIsCubemap
+            // 
+            this.colIsCubemap.Text = "Cubemap";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(952, 418);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "frmMain";
+            this.Text = "DDS Texture Scanner v1.0 by niston";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel tslStatus;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtScanPath;
+        private System.Windows.Forms.ListView lsvResults;
+        private System.Windows.Forms.ColumnHeader colFilePath;
+        private System.Windows.Forms.ColumnHeader colDimensions;
+        private System.Windows.Forms.ColumnHeader colMipmapCount;
+        private System.Windows.Forms.ColumnHeader colIsCubemap;
+    }
+}
+
---
+++ TextureScan/frmMain.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextureScan
+{
+    public partial class frmMain : Form
+    {
+        // DDS file format info
+        // https://learn.microsoft.com/en-us/windows/win32/direct3ddds/dds-header
+
+        // DDS cubemap flag
+        private const int DDSCAPS2_CUBEMAP = 0x200;
+        
+        // DDS header struct        
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        private struct DDSHeader
+        {
+            public uint size;		        // must be 124 (size of DDSHeader struct)
+            public uint flags;
+            public uint height;
+            public uint width;
+            public uint sizeorpitch;
+            public uint depth;
+            public uint mipmapcount;
+            public uint alphabitdepth;
+            public uint[] reserved;
+
+            [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+            public struct DDSPixelFormat
+            {
+                public uint size;
+                public uint flags;
+                public uint fourcc;
+                public uint rgbbitcount;
+                public uint rbitmask;
+                public uint gbitmask;
+                public uint bbitmask;
+                public uint alphabitmask;
+            }
+            public DDSPixelFormat pixelformat;
+
+            [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+            public struct DDSCaps
+            {
+                public uint caps1;
+                public uint caps2;
+                public uint caps3;
+                public uint caps4;
+            }
+            public DDSCaps ddscaps;
+            public uint texturestage;
+        }
+
+        // abort flag
+        bool bAbortScan = false;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private DDSHeader ReadHeader(string filePath)
+        {
+            // header struct
+            DDSHeader header;
+            
+            // texture file stream
+            using (FileStream stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                // filestream binary reader
+                using (BinaryReader reader = new BinaryReader(stream))
+                {                    
+                    // read and verify DDS magic
+                    byte[] signature = reader.ReadBytes(4);
+                    if (!(signature[0] == 'D' && signature[1] == 'D' && signature[2] == 'S' && signature[3] == ' '))
+                        throw new FormatException("Not a DDS file (" + filePath + "): Magic Not Found.");
+
+                    // read and verify DDS header size
+                    header.size = reader.ReadUInt32();
+                    if (header.size != 124)
+                        throw new FormatException("Not a DDS file (" + filePath + "): Unexpected Header Struct Size.");
+
+                    // read main header fields
+                    header.flags = reader.ReadUInt32();
+                    header.height = reader.ReadUInt32();
+                    header.width = reader.ReadUInt32();
+                    header.sizeorpitch = reader.ReadUInt32();
+                    header.depth = reader.ReadUInt32();
+                    header.mipmapcount = reader.ReadUInt32();
+                    header.alphabitdepth = reader.ReadUInt32();
+
+                    // read 10 reserved bytes
+                    header.reserved = new uint[10];
+                    for (int i = 0; i < 10; i++)
+                    {
+                        header.reserved[i] = reader.ReadUInt32();
+                    }
+
+                    // read pixel format field
+                    header.pixelformat.size = reader.ReadUInt32();
+                    header.pixelformat.flags = reader.ReadUInt32();
+                    header.pixelformat.fourcc = reader.ReadUInt32();
+                    header.pixelformat.rgbbitcount = reader.ReadUInt32();
+                    header.pixelformat.rbitmask = reader.ReadUInt32();
+                    header.pixelformat.gbitmask = reader.ReadUInt32();
+                    header.pixelformat.bbitmask = reader.ReadUInt32();
+                    header.pixelformat.alphabitmask = reader.ReadUInt32();
+
+                    // read DDS capabilities fields
+                    header.ddscaps.caps1 = reader.ReadUInt32();
+                    header.ddscaps.caps2 = reader.ReadUInt32();
+                    header.ddscaps.caps3 = reader.ReadUInt32();
+                    header.ddscaps.caps4 = reader.ReadUInt32();
+
+                    // read texture stage field
+                    header.texturestage = reader.ReadUInt32();
+                    
+                    // done
+                    reader.Close();
+                }
+                stream.Close();
+            }
+
+            return header;
+        }
+
+        // main function
+        private void ScanFolder()
+        {
+            try
+            {
+                bAbortScan = false;
+
+                // clear list
+                lsvResults.Items.Clear();                
+
+                // recursively iterate dds files in path
+                tslStatus.Text = "Enumerating texture files in path...";
+                Application.DoEvents();
+
+                // enumerate texture files
+                string[] filePaths = Directory.GetFiles(txtScanPath.Text, "*.dds", SearchOption.AllDirectories);
+
+                // record scan start time
+                DateTime startTime = DateTime.Now;
+
+                // scan enumerated files
+                int fileCount = 0;
+
+                foreach (string filePath in filePaths)
+                {
+                    // exit foreach if abort flag is set
+                    if (bAbortScan) { break; }
+
+                    // increase filecount
+                    fileCount += 1;
+
+                    // UI chatter stuff every 11th file                    
+                    if (fileCount % 11 == 0)
+                    {
+                        tslStatus.Text = "Scanning texture (" + fileCount.ToString() + "/" + filePaths.Length.ToString() + "): " + Path.GetFileName(filePath) + "...";
+                        Application.DoEvents();                     
+                    }
+
+                    try
+                    {
+                        // read DDS header
+                        DDSHeader ddsHeader = ReadHeader(filePath);
+                        
+                        // validate dimensions
+                        if (!IsValidSize(ddsHeader))
+                        {
+                            var newItem = lsvResults.Items.Add(filePath);
+                            newItem.SubItems.Add(ddsHeader.width.ToString() + " x " + ddsHeader.height.ToString());
+                            newItem.SubItems.Add(ddsHeader.mipmapcount.ToString());
+                            newItem.SubItems.Add(IsBitSet(ddsHeader.ddscaps.caps2, DDSCAPS2_CUBEMAP) ? "YES" : "NO");
+                        }
+
+                    }
+                    catch (FormatException fEx)
+                    {
+                        // file is not a valid dds
+                        var newItem = lsvResults.Items.Add(filePath);
+                        newItem.SubItems.Add("INVALID");
+                    }
+                    catch (IndexOutOfRangeException iorEx)
+                    {
+                        // file has malformed dds header
+                        var newItem = lsvResults.Items.Add(filePath);
+                        newItem.SubItems.Add("CORRUPTED");
+                    }
+                }
+
+                tslStatus.Text = "Scanned (" + fileCount + ") textures in (" + DateTime.Now.Subtract(startTime).TotalSeconds + ") seconds.";
+            }
+            catch (Exception ex)
+            {
+                HandleException(ex);
+            }
+        }
+
+        // open file with default program
+        public static void OpenWithDefaultProgram(string path)
+        {
+            using (Process fileopener = new Process())
+            {
+                fileopener.StartInfo.FileName = "explorer";
+                fileopener.StartInfo.Arguments = "\"" + path + "\"";
+                fileopener.Start();
+            }
+        }
+
+        // validate texture dimensions from DDSHeader
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private bool IsValidSize(DDSHeader header)
+        {
+            // cubemap flag set ?
+            if (IsBitSet(header.ddscaps.caps2, DDSCAPS2_CUBEMAP))
+
+            {
+                // is a cubemap, dimensions must be multiple of 4
+                return IsDivisible(header.width, 4) && IsDivisible(header.height, 4);
+            }
+            else
+            {
+                // not a cubemap, dimensions must be power of 2
+                return IsPow2(header.width) && IsPow2(header.height);
+            }
+        }
+
+        // validation helpers
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private bool IsBitSet<T>(T t, int pos) where T : struct, IConvertible
+        {
+            var value = t.ToInt64(CultureInfo.CurrentCulture);
+            return (value & (1 << pos)) != 0;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private bool IsPow2(uint n)
+        {
+            return (n & (n - 1)) == 0 && n > 0;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool IsDivisible(uint x, uint n)
+        {
+            return (x % n) == 0;
+        }
+
+        // UI functions
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            bAbortScan = true;
+            Properties.Settings.Default.ScanPath = txtScanPath.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            txtScanPath.Text = Properties.Settings.Default.ScanPath;
+        }
+
+        private void lsvResults_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (lsvResults.Items.Count > 0)
+                {
+                    String filePath = lsvResults.SelectedItems[0].Text;
+                    OpenWithDefaultProgram(filePath);
+                }
+            }
+            catch (Exception ex)
+            {
+                HandleException(ex);
+            }
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FolderBrowserDialog fDlg = new FolderBrowserDialog();
+                fDlg.SelectedPath = txtScanPath.Text;
+                if (fDlg.ShowDialog() == DialogResult.OK)
+                {
+                    txtScanPath.Text = fDlg.SelectedPath;
+                }
+            }
+            catch (Exception ex)
+            {
+                HandleException(ex);
+            }
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            ScanFolder();
+        }
+
+        private void HandleException(Exception ex)
+        {
+            MessageBox.Show(this, ex.Message, "Exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
